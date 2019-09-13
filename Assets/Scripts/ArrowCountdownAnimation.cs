61a2373979646340bbf60c0ba4f27dd024f528d9
Murilo Trigo <murilotrigopaz@gmail.com>
2019-09-12 22:20:31 -0400

    Adds the Title Screen arrow countdown animation

---
+++ Assets/Scripts/ArrowCountdownAnimation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowCountdownAnimation : MonoBehaviour
+{
+    public Sprite arrowSprite;
+    public Sprite arrowMissingSprite;
+
+    public GameObject[] arrowCounters;
+
+    public float duration;
+    public float fadeTime;
+
+    private float elapsed = 0;
+    private int alpha = 1;
+    private int reverseIndex = 0;
+
+    private int Length { get => arrowCounters.Length;  }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        elapsed += Time.deltaTime;
+
+        if ( elapsed > duration )
+        {
+            elapsed %= duration;
+
+            if ( reverseIndex < Length )
+            {
+                GameObject counter = arrowCounters[ Length - reverseIndex - 1 ];
+                counter.GetComponent<SpriteRenderer>().sprite = arrowMissingSprite;
+                reverseIndex++;
+            }
+            else
+            {
+                reverseIndex = 0;
+                foreach( SpriteRenderer spriteRenderer in GetComponentsInChildren<SpriteRenderer>() )
+                {
+                    spriteRenderer.sprite = arrowSprite;
+                }
+            }
+
+        }
+    }
+}
