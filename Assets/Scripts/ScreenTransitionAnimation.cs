d8c8f7344c9ffd8c9676c2fb782d465db426e9ef
Murilo Trigo <murilotrigopaz@gmail.com>
2020-02-24 00:41:30 -0500

    Implement scene transitions (always swipe to left)

---
+++ Assets/Scripts/ScreenTransitionAnimation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ScreenTransitionAnimation : MonoBehaviour
+{
+    void Awake()
+    {
+        AnimateSceneStart();
+    }
+
+    public void AnimateSceneStart()
+    {
+        GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    void Update()
+    {
+        Vector2 endPosition = Vector2.left * 30;
+        Vector2 vector = endPosition - ( Vector2 ) transform.position;
+        if ( vector.magnitude < 0.1f )
+        {
+            gameObject.SetActive( false );
+        }
+
+        transform.position = Vector3.MoveTowards( transform.position, endPosition, 80 * Time.smoothDeltaTime );
+    }
+}
